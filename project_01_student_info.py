b33bf1b21950650949e7f4e52e714c84c1b8c820
Rushika Patel <rushikapatel1119@gmail.com>
2026-06-19 23:16:04 +0530

    Added Student Information System project

---
+++ project_01_student_info.py
@@ -0,0 +1,18 @@
+"""
+Project 01: Student Information System
+Author: Rushika Patel
+
+This program collects student information
+and displays it in a formatted way.
+"""
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print("\n===== STUDENT INFORMATION =====")
+print("Name :", name)
+print("Age  :", age)
+print("City :", city)
+print("===============================")
+
