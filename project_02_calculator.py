5f08295d82f64318be79074ceaa0a793d64a4d08
Rushika Patel <rushikapatel1119@gmail.com>
2026-06-20 20:18:14 +0530

    Added Simple Calculator project

---
+++ project_02_calculator.py
@@ -0,0 +1,19 @@
+"""
+Project 02: Simple Calculator
+Author: Rushika Patel
+
+This program performs basic arithmetic operations
+on two numbers entered by the user.
+"""
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\n===== CALCULATOR RESULTS =====")
+
+print("Addition       :", num1 + num2)
+print("Subtraction    :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division       :", num1 / num2)
+
+print("==============================")
